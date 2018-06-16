931ebc959b2a12da64667eff6ec94726787d2cee
John Lees-Miller <jdleesmiller@gmail.com>
2018-06-16 21:27:55 +0100

    Update deploy script to support multiple languages

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #
 # Copy the slides and the exercise files referenced from the slides from the
 # master branch to the gh-pages branch to make them accessible at
@@ -9,22 +11,30 @@ if ! git diff-index --quiet HEAD --; then
   exit
 fi
 
+set -ex
+
+LANGUAGES="en fr"
+
 # build / bundle files that we need to put on the website
-cd en
-make latex-course.tgz
-cd ..
+for language in $LANGUAGES; do
+  cd "$language"
+  make latex-course.tgz
+  cd ..
+done
 
 # stage the website in the deploy directory
 rm -rf deploy
 mkdir deploy
 cd deploy
 
-mkdir en
-cd en
-mv ../../en/latex-course.tgz .
-tar xzf latex-course.tgz
-rm latex-course.tgz
-cd ..
+for language in $LANGUAGES; do
+  mkdir "$language"
+  cd "$language"
+  mv "../../$language/latex-course.tgz" .
+  tar xzf latex-course.tgz
+  rm latex-course.tgz
+  cd ..
+done
 
 echo '<html><head><title>latex-course</title></head><body><a href="https://github.com/jdleesmiller/latex-course">Please see repository.</a></body></html>' > index.html
 
@@ -33,7 +43,9 @@ cd ..
 # destroy current content of the gh-pages branch
 git checkout gh-pages
 git rm -rf .
-rm -rf en
+for language in $LANGUAGES; do
+  rm -rf "$language"
+done
 mv deploy/* .
 rmdir deploy
 git add .
